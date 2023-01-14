f5f4720ebf815490d149df00305b1f1975af0788
Artem Kostornov <ArtKosto@yandex.ru>
2023-01-14 18:18:30 +0300

    add Task4

---
+++ Task4.cs
@@ -0,0 +1,30 @@
+
+Console.WriteLine("Введите число 1: ");
+int number1 = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int number2 = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 3: ");
+int number3 = Int32.Parse(Console.ReadLine());
+
+
+if (number1 > number2)
+{
+    if (number1 > number3)
+    {
+        Console.WriteLine($" max= {number1}");
+    }
+}
+else
+{
+   if (number2 > number3)
+   {
+    Console.WriteLine($" max= {number2} ");
+   } 
+   else
+   {
+    Console.WriteLine($" max= {number3}");
+   }
+}
+
